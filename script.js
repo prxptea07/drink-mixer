90f444c3e35047c6e3a0c5d9d48286178a5a38ff
prxptea07 <samihaprapti43@gmail.com>
2025-07-19 22:26:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let stackCount = 0;
+const ingredients = document.querySelectorAll(".ingredient");
+const dropZone = document.getElementById("drop-zone");
 
-function dropIngredient(type) {
-  const ingredient = document.createElement('img');
-  ingredient.src = `${type}.png`;
-  ingredient.classList.add('ingredient');
-  document.getElementById('glass-contents').appendChild(ingredient);
-}
+ingredients.forEach((item) => {
+  item.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = item.src;
+    img.classList.add("falling");
 
-  stackCount++;
-}
+    // Position the new item at the top of the glass
+    img.style.left = Math.random() * 50 + 25 + "%";
+
+    dropZone.appendChild(img);
+
+    img.addEventListener("animationend", () => {
+      img.classList.remove("falling");
+      img.classList.add("in-glass");
+    });
+  });
+});
+
